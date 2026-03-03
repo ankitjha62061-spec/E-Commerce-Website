095bbb549ae0c61070695bed7ee8be5904e67e99
Ankit  Jha <ankitjha62061@gmail.com>
2026-03-03 12:52:05 +0530

    Make a folder structure for website like componets,context and pages for everything

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 const App = () => {
   return (
     <div>
-      <h1 className="bg-blue-500 text-white p-4" >Hello</h1>
+      <h1 className="bg-green-500 text-white p-4" >Hello</h1>
 
     </div>
   )
