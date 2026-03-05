e002d53e20c47c3c32b9cd001010bd9ded84f1d5
Ankit  Jha <ankitjha62061@gmail.com>
2026-03-05 17:40:20 +0530

    add navbar a new side bar for small screen when user open the website its open a new side bar  menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {assets} from '../assets/assets'
 import { Link, NavLink } from 'react-router-dom'
 
 const Navbar = () => {
+
+const [visible, setVisible] =useState(false)
+
   return (
     <div className= 'flex items-center justify-between py-5 font-medium'>  
 
@@ -40,7 +43,7 @@ const Navbar = () => {
 <div className='flex items-center gap-6'>
 <img src={assets.search_icon} className='w-5 cursor-pointer' alt="" />
 
-<div className='group realtive'>
+<div className='group relative'>
 <img  className='w-5 cursor-pointer' src={assets.profile_icon} alt="" />
 
 <div className='group-hover:block hidden absolute dropdown-menu right-0 pt-4' >
@@ -64,8 +67,28 @@ const Navbar = () => {
 
 </Link>
 
+<img onClick={()=>setVisible(true)} src={assets.menu_icon} className='w-5 cursor-pointer sm:hidden' alt="" />
+</div>
+
+{/* slidebar for small screen  */}
+
+{/* <div className={`absoulute  top-0 right-0 bottom-0 overflow-hidden bg-white transition-all ${visible ? 'w-full': 'w-0'}`}> */}
+  <div className={`absolute top-0 right-0 bottom-0 overflow-hidden bg-white transition-all ${visible ? 'w-full' : 'w-0'}`}>
+
+<div className='flex flex-col text-gray-600'>
+  <div onClick={()=>setVisible(false)}  className='flex items-center gap-4 p-3 cursor-pointer'>
+    <img className='h-4 rotate-180' src={assets.dropdown_icon} alt="" />
+<p>Back</p>
+  </div>
+<NavLink onClick={()=>setVisible(false)}  className='py-2 pl-6 border'  to='/'>HOME</NavLink>
+<NavLink onClick={()=>setVisible(false)}  className='py-2 pl-6 border' to='/collection'>COLLECTION</NavLink>
+<NavLink onClick={()=>setVisible(false)}  className='py-2 pl-6 border' to='/about'>ABOUT</NavLink>
+<NavLink onClick={()=>setVisible(false)}  className='py-2 pl-6 border' to='/contact'>CONTACT</NavLink>
 
 </div>
+
+</div>
+
 </div>
   )
 }
