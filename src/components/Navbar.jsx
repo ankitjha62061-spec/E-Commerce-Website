237719790f68a524a876ddf235c745bc604a0314
Ankit  Jha <ankitjha62061@gmail.com>
2026-03-05 13:57:21 +0530

    design navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import {assets} from '../assets/assets'
-import { NavLink } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 
 const Navbar = () => {
   return (
@@ -33,13 +33,40 @@ const Navbar = () => {
 <p>CONTACT</p>
 <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hidden' />
 </NavLink>
-
-
-
 </ul>
 
-    </div>
+
+
+<div className='flex items-center gap-6'>
+<img src={assets.search_icon} className='w-5 cursor-pointer' alt="" />
+
+<div className='group realtive'>
+<img  className='w-5 cursor-pointer' src={assets.profile_icon} alt="" />
+
+<div className='group-hover:block hidden absolute dropdown-menu right-0 pt-4' >
+
+<div className='flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500 rounded'> 
+
+<p className='cursor-pointer hover:text-black'>My profile</p>
+<p className='cursor-pointer hover:text-black'>Orders</p>
+<p className='cursor-pointer hover:text-black'> Logout</p>
+
+
+
+</div>
+</div>
+</div>
+
+<Link to='/cart' className='realtive'>
+
+<img src={assets.cart_icon} className='w-5 min-w-5' alt="" />
+<p className='absolute'></p>
+
+</Link>
+
+
+</div>
+</div>
   )
 }
-
 export default Navbar
