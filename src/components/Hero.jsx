62ab8526aaeb1ecd6b416b5bc8c42feb1af1ae4c
Ankit  Jha <ankitjha62061@gmail.com>
2026-03-06 15:18:05 +0530

    add a hero section in ui add new fonts and pictures of  shop now

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import { assets } from '../assets/assets'
 
 const Hero = () => {
   return (
     <div className='flex flex-col sm:flex-row border border-gray-400 '>
-        
-      <div className='w-full sm:w-1/2 flex items-center justify-center py-10 sm:py-0'>  
+        {/* hero left side  */}
+<div className='w-full sm:w-1/2 flex items-center justify-center py-10 sm:py-0'>  
 
 <div className='text-[#414141]'>
 
@@ -13,13 +14,18 @@ const Hero = () => {
 <p className='w-8 md:w-11 h-[2px] bg-[#414141]  '></p>
 <p className='font-medium text-sm md:text-base'>OUR BESTSELLERS</p>
 </div>
-<h1 className=' text-3xl sm:py-3 lg:text-5xl leading-relaxed'> Latest <Arrivals></Arrivals> </h1>
+<h1 className='  .prata-regular text-3xl sm:py-3 lg:text-5xl leading-relaxed'> Latest</h1>
 <div className='flex items-center gap-2'>
-<p className='font-semibold text-sm md:text-base'></p>
+<p className='font-semibold text-sm md:text-base'> SHOP NOW </p>
+<p className='w-8 md:w-11 h-[1px] bg-[#414141] ' ></p>
 </div>
 
 </div>
  </div>
+
+{/* hero right side */}
+<img className='w-full sm:w-1/2' src={assets.hero_img} alt="" />
+
  </div>
   )
 }
