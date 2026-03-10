19f5b333e2102f08806ccd2dce476891f473e98d
Ankit  Jha <ankitjha62061@gmail.com>
2026-03-10 16:35:08 +0530

    add a collection page

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,13 +10,45 @@ const Footer = () => {
           <img src={assets.logo} className='mb-5 w-32' alt="" />
 
           <p className='w-full md:w-2/3 text-gray-600'>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-            Atque, molestiae iure? Consectetur sunt maxime facilis, 
-            cumque repudiandae quisquam autem facere.
-          </p>
+        YourStore is your trusted online shopping destination for quality products at the best prices. 
+        We offer a wide range of fashion, electronics, and lifestyle products with fast delivery and 
+        excellent customer support to make your shopping experience easy and enjoyable.
+</p>
         </div>
 
-      </div>
+
+<div>
+
+ <p className='text-xl font-medium mb-5 '>COMPANY</p>
+ <ul className='flex flex-col gap-1 text-gray-600'>
+
+<li>HOME</li>
+<li>About</li>
+<li>Delivery</li>
+<li>Privacy</li>
+ </ul>   
+</div>
+
+<div>
+    <p className='text-xl font-medium mb-5'>GET IN TOUCH </p>
+     <ul className='flex flex-col gap-1 text-gray-600'>
+
+     <li>+918256412952</li>
+     <li>contact@forverjha.com</li>
+
+     </ul>
+
+     </div>
+ </div>
+
+<div>
+<hr />
+<p className='py-5 text-sm text-center'>© 2026 Forever. All Rights Reserved.</p>
+
+</div>
+
+
+
     </div>
   )
 }
--- src/components/NewsletterBox.jsx
+++ src/components/NewsletterBox.jsx
@@ -10,8 +10,9 @@ event.preventDefault();
   return (
     <div className='text-center'>
 <p className='text-2xl font-medium text-gray-800' > Subscribe Now & get 20% off </p>
-<p className='text-gray-400 mt-3'> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ex architecto odit, distinctio earum beatae minus voluptatum voluptas quisquam omnis dolore, sapiente aspernatur, maiores numquam veniam deleniti quo ipsa quos corrupti.</p>
-  
+<p className='text-black-400 mt-3'>
+Subscribe to our newsletter and be the first to know about new arrivals, exclusive offers, and special discounts.
+</p>
    <form onSubmit={onSubmitHandler} className='w-full sm:w-1/2 flex items-center gap-3 mx-auto my-6 border pl-3'>
     <input className='w-full sm:flex-1 outline-none' type="email" placeholder='Enter your email' />
   <button type='submit' className='bg-black text-white text-xs px-10 py-4' >SUBSCRIBE</button>
