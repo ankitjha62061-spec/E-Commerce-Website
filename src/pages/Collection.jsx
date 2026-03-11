51eb14083ab86d781f250d527fef9993590a2292
Ankit  Jha <ankitjha62061@gmail.com>
2026-03-11 13:15:13 +0530

    add a filter option for small icon when user click the filter show categories and types

--- src/pages/Collection.jsx
+++ src/pages/Collection.jsx
@@ -1,5 +1,6 @@
 import React, { useContext, useState } from 'react'
 import { ShopContext } from '../context/ShopContext'
+import { assets } from '../assets/assets';
 
 const Collection = () => {
 
@@ -9,40 +10,48 @@ const [showFilter,setShowFilter] = useState(false);
 
   return (
     <div className='flex flex-col sm:flex-row gap-1 sm:gap-10 pt-10 border-t'>
-           {/*filter option  */}
+    
+    {/*filter option  */}
 
-          <div className='min-w-60' >
-<p className='my-2 text-xl flex items-center cursor-pointer gap-2'>FILTERS</p>
+<div className='min-w-60' >
+<p onClick={()=> setShowFilter(!showFilter)} className='my-2 text-xl flex items-center cursor-pointer gap-2'>FILTERS
+  
+  <img className= {`h-3 sm:hidden ${showFilter ? 'rotate-90' : '' }`} src=  {assets.dropdown_icon} alt="" />
+</p>
+
+
 {/* category filter */}
-
-
 
 <div className={`border border-gray-300 pl-5 py-3 mt-6 ${showFilter ? '' : 'hidden'} sm:block`}> 
   <p className='mb-3 text-sm font-medium'>CATEGORIES</p>
 <div className='flex flex-col gap-2 text-sm font-light text-gray-700'>
+
 <p className='flex gap-2'>
   <input className='w-3 '  type="checkbox" value={'Men'} /> Men
 </p>
+
 
 <p className='flex gap-2'>
   <input className='w-3 '  type="checkbox" value={'Woman'} /> Woman
 </p>
 
+
 <p className='flex gap-2'>
   <input className='w-3 '  type="checkbox" value={'Kids'} /> Kids
 </p>
 
+
 </div>
 </div>
+
 
 
  {/* SubCategory Filter  */}
-
-<div className={`border border-gray-300 pl-5 py-3 mt-6 ${showFilter ? '' : 'hidden'} sm:block`}> 
+<div className={`border border-gray-300 pl-5 py-3 my-5 ${showFilter ? '' : 'hidden'} sm:block`}> 
   <p className='mb-3 text-sm font-medium'>TYPES</p>
 <div className='flex flex-col gap-2 text-sm font-light text-gray-700'>
 <p className='flex gap-2'>
-  <input className='w-3 '  type="checkbox" value={'Topwear'} /> Toopwear
+  <input className='w-3 '  type="checkbox" value={'Topwear'} /> Topwear
 </p>
 
 <p className='flex gap-2'>
@@ -55,6 +64,7 @@ const [showFilter,setShowFilter] = useState(false);
 
 </div>
 </div>
+
 
 
 </div>
